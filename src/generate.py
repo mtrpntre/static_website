ca166dfff7576971286a0752cc55aed77e1a15e4
mtrpntre <schaub.nelson@gmail.com>
2025-03-04 00:45:43 +0100

    everything sorry

---
+++ src/generate.py
@@ -0,0 +1,35 @@
+from markdown_blocks import extract_title
+from markdown_blocks import markdown_to_html_node
+import os
+from pathlib import Path
+def generate_page(from_path, template_path, dest_path):
+    print(f"generating page from {from_path} to {dest_path} using {template_path}")
+    with open(from_path, "r") as f:
+        markdown = f.read()
+    
+    with open(template_path, "r") as f:
+        template = f.read()
+
+    title = extract_title(markdown)
+    markdown_html = markdown_to_html_node(markdown)
+    content = markdown_html.to_html()
+
+    template = template.replace("{{ Title }}", title)
+    template = template.replace('''{{ Content }}''', content)
+
+    
+
+
+    if not os.path.exists(os.path.dirname(dest_path)):
+        os.makedirs(os.path.dirname(dest_path))
+    with open(dest_path, "w") as f:
+        f.write(template)
+
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+    for path in Path(dir_path_content).rglob('*.md'):
+        from_path = str(path)
+        dest_path = from_path.replace(dir_path_content, dest_dir_path).replace(".md", ".html")
+        generate_page(from_path, template_path, dest_path)
+
+
+
---
+++ src/main.py
@@ -0,0 +1,39 @@
+from textnode import *
+from markdown_blocks import *
+from inline_markdown import *
+from htmlnode import *
+from generate import *
+import shutil
+import os
+
+def from_static_to_public(source_directory, destination_directory):
+
+    os.makedirs(destination_directory, exist_ok=True)
+    for item in os.listdir(source_directory):
+        source_item = os.path.join(source_directory, item)
+        destination_item = os.path.join(destination_directory, item)
+        if os.path.isdir(source_item):
+            from_static_to_public(source_item, destination_item)
+        else:
+            shutil.copy2(source_item, destination_item)
+
+
+def delete_directory_content(directory):
+    for item in os.listdir(directory):
+        item_path = os.path.join(directory, item)
+        if os.path.isdir(item_path):
+            shutil.rmtree(item_path)
+        else:
+            os.remove(item_path)
+
+def main():
+    delete_directory_content("public")
+    from_static_to_public("static", "public")
+    generate_pages_recursive("content", "template.html", "public")
+    
+    
+
+        
+    
+if __name__ == "__main__":
+    main()
